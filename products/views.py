3043a67a18d04a31ce467eb741925b14a9a8dd40
totalnoMartina <totalnobzvz@yahoo.com>
2022-03-28 00:00:42 +0000

    Add product informations, smooth scroll

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -10,3 +10,12 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_info(request, product_id):
+    """ Rendering detailed information on the product """
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product
+    }
+
+    return render(request, 'products/product_info.html', context)
